7ea5bdd660183f4fbfd3a6d53092671edddc617d
Hizami Rashid <mohammad.norhizami.mohd@rhbgroup.com>
2024-11-23 23:47:58 +0800

    dependencies add

--- Package.swift
+++ Package.swift
@@ -6,23 +6,23 @@ import PackageDescription
 let package = Package(
     name: "HudHudSDK",
     platforms: [
-          .iOS(.v16)
-        ],
-        products: [
-            // Products define the executables and libraries a package produces, and make them visible to other packages.
-            .library(
-                name: "HudHudSDK",
-                targets: ["HudHudSDK"]),
-        ],
-        dependencies: [
-            // Dependencies declare other packages that this package depends on.
-            // .package(url: /* package url */, from: "1.0.0"),
-        ],
-        targets: [
-            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-            // Targets can depend on other targets in this package, and on products in packages this package depends on.
-            .binaryTarget(
-                name: "HudHudSDK",
-                path: "./Sources/HudHudSDK.xcframework")
-        ]
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "HudHudSDK",
+            targets: ["HudHudSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1"),
+        .package(url: "https://github.com/SwiftfulThinking/SwiftfulRouting.git", from: "5.3.6")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "HudHudSDK",
+            path: "./Sources/HudHudSDK.xcframework")
+    ]
 )
