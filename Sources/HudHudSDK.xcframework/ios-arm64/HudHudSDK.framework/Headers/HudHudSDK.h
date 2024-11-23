910dcc4c316d9f0789b0576fef139123d2a0af8e
Hizami Rashid <mohammad.norhizami.mohd@rhbgroup.com>
2024-11-23 23:40:48 +0800

    initial commit

---
+++ Sources/HudHudSDK.xcframework/ios-arm64/HudHudSDK.framework/Headers/HudHudSDK.h
@@ -0,0 +1,18 @@
+//
+//  HudHudSDK.h
+//  HudHudSDK
+//
+//  Created by Hizami Rashid on 23/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HudHudSDK.
+FOUNDATION_EXPORT double HudHudSDKVersionNumber;
+
+//! Project version string for HudHudSDK.
+FOUNDATION_EXPORT const unsigned char HudHudSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HudHudSDK/PublicHeader.h>
+
+
